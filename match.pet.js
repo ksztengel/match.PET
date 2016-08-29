1f07e17658108f79baf6e6a59224aa0f17cf4d77
Kristin Sztengel <kcsztengel@gmail.com>
2016-08-29 08:56:59 -0600

    api url and user input error/success messages

--- match.pet.js
+++ match.pet.js
@@ -1,29 +1,32 @@
 $(document).ready(function() {
-    // $('select').material_select();
     console.log("ready!");
-    $('.btn').on('click', function(event) {
-        // event.preventDefault();
-        // if($("#zip_code").val() === ''){
-        //   Materialize.toast('Please enter a zip code.', 4000)
-          console.log("it clicked");
-        })
-        var location = $('#zip_code').val();
-        // function changeSearchZip(zip) {
-        //   searchZip = zip;
-        var animal = $('#pets').val()
-        // }
-        // $.ajax({
-        //   method: 'GET',
-        //   url: 'http://api.petfinder.com/my.method?pet.find?key=802794cfd500084ba38b444334194949&location=11937&full&animal=dog',
-      //     dataType: 'json',
-      // success: function(){
-// }
-      // error: function(err) {
-      //   console.log('error', err);
-      // }
-      $.getJSON('http://api.petfinder.com/pet.find?format=json&key=802794cfd500084ba38b444334194949&location=11937&animal=dog&callback=?')
-      .done(function(petApiData) { alert('Data retrieved!'); })
-      .error(function(err) { alert('Error retrieving data!');
+  $('.btn').on('click', function(event) {
+      if ($('input[name=zip_code').val().length === 0){
+        Materialize.toast('Please enter a zip code.', 4000)
+      } else if($('input[name=pets').val().length === 0){
+        Materialize.toast('Please enter dog or cat.', 4000)
+      } else {
+          Materialize.toast('Thank you!', 4000)
+          event.preventDefault();
+      }
+
+      console.log("it clicked");
+      var zip_code = $('input[name=zip_code').val()
+      var pets = $('input[name=pets').val()
+      console.log('location',zip_code);
+      console.log('pets', pets);
+
+    $.getJSON("http://api.petfinder.com/pet.find?format=json&key=802794cfd500084ba38b444334194949&location=" + zip_code + "&animal=" + pets + "&callback=?")
+    .done(function(petApiData) { console.log('Data retrieved!');
+      console.log('data',petApiData)
+    })
+      .fail(function(err) { alert('Error retrieving data!');
+      // for(var i=0; i <petApiData.length; i++){
+      //       let thing = petApiData[i]
+      //       console.log(thing.location);
+      //       console.log(thing.animal);
+      //       console.log(thing.name);
       })
     })
-      // })
+  })
+// })
