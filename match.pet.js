a2a6f08e281b1938827f5446e44f69debfe86c4c
Kristin Sztengel <kcsztengel@gmail.com>
2016-08-31 12:25:35 -0600

    created dataCall function

--- match.pet.js
+++ match.pet.js
@@ -18,13 +18,21 @@ $(document).ready(function() {
       event.preventDefault();
     }
 
+    dataCall()
+
+  })
+
+
+  function dataCall(){
     const zipCode = $('input[name=zipCode').val()
     const pets = $('#pets').val()
 
     $.getJSON("http://api.petfinder.com/pet.find?format=json&key=802794cfd500084ba38b444334194949&location=" + zipCode + "&animal=" + pets + "&offset=" + offset + "&count=24&callback=?")
     .done(function(data) {
-      const petList = data.petfinder.pets.pet
 
+      console.log('data', data)
+
+      const petList = data.petfinder.pets.pet
       offset = data.petfinder.lastOffset.$t
 
       for (let i = 0; i < petList.length; i++) {
@@ -39,45 +47,47 @@ $(document).ready(function() {
         const tempMedia = pet.media.photos.photo[0].$t;
         const media = tempMedia.split('&')
         const image = media[0] + '.jpg'
+        }
+      })
 
-        $('#petContainer').append(`
-          <div class="col s3 m3">
-            <div class="card small z-depth-4">
-              <div class="card-image">
-                <img src="${image}">
-                  <span class="card-title"></span>
-          </div>
-              <div class="card-content">
-                <p>${name}</p>
-              </div>
-                <div class="card-action">
-  <a class="modal-trigger waves-effect waves-light btn #795548 brown" href="#modal${i}">Learn more!</a>
-  <div id="modal${i}" class="modal modal-fixed-footer">
-    <div class="modal-content">
-    <div class="card small" id="popUp">
-    <div class="card-image">
-        <img src=${image}>
-      </div>
-      </div>
-      <h4>${name}: ${age} ${breed} (${sex})</h4>
-      <p>${description}</p>
-      <h5>Please contact the shelter:  ${email}</h5>
-      <h5>${phone}</h5>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat"><h4>X</h4></a>
-    </div>
-  </div>
-              </div>
-            </div>
-          </div>
-          `)
-      }
-      $('.modal-trigger').leanModal();
-
-      console.log('data', data)
-    })
     .fail(function() {
     })
-  })
+  }
+  //nextButton function hear
+  $('.modal-trigger').leanModal();
 })
+
+
+
+//       $('#petContainer').append(`
+//         <div class="col s3 m3">
+//           <div class="card small z-depth-4">
+//             <div class="card-image">
+//               <img src="${image}">
+//                 <span class="card-title"></span>
+//         </div>
+//             <div class="card-content">
+//               <p>${name}</p>
+//             </div>
+//               <div class="card-action">
+// <a class="modal-trigger waves-effect waves-light btn #795548 brown" href="#modal${i}">Learn more!</a>
+// <div id="modal${i}" class="modal modal-fixed-footer">
+//   <div class="modal-content">
+//   <div class="card small" id="popUp">
+//   <div class="card-image">
+//       <img src=${image}>
+//     </div>
+//     </div>
+//     <h4>${name}: ${age} ${breed} (${sex})</h4>
+//     <p>${description}</p>
+//     <h5>Please contact the shelter:  ${email}</h5>
+//     <h5>${phone}</h5>
+//   </div>
+//   <div class="modal-footer">
+//     <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat"><h4>X</h4></a>
+//   </div>
+// </div>
+//             </div>
+//           </div>
+//         </div>
+//         `)
