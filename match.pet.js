ac2de3ec99f36318cc9ba04649b1db0973e700e0
Kristin Sztengel <kcsztengel@gmail.com>
2016-08-31 10:58:40 -0600

    dropdown and error, clear fields, next buttion, css for modul

--- match.pet.js
+++ match.pet.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
   'use strict'
+  $('select').material_select();
   $('.modal-trigger').leanModal();
-
 
   let offset = 0;
 
-  $('.btn').on('click', function(event) {
+  $('#matchButton').on('click', function(event) {
+    $("#petContainer").empty()
     if ($('input[name=zipCode').val().length === 0) {
       Materialize.toast('Please enter a zip code.', 4000);
     }
-    else if ($('input[name=pets').val().length === 0) {
-      Materialize.toast('Please enter dog or cat.', 4000);
+    else if ($('#pets').val() === null) {
+      Materialize.toast('Please choose dog or cat.', 4000);
     }
     else {
       Materialize.toast('Thank you!', 4000)
@@ -18,41 +19,13 @@ $(document).ready(function() {
     }
 
     const zipCode = $('input[name=zipCode').val()
-    const pets = $('input[name=pets').val()
-
+    const pets = $('#pets').val()
 
     $.getJSON("http://api.petfinder.com/pet.find?format=json&key=802794cfd500084ba38b444334194949&location=" + zipCode + "&animal=" + pets + "&offset=" + offset + "&count=24&callback=?")
     .done(function(data) {
       const petList = data.petfinder.pets.pet
 
       offset = data.petfinder.lastOffset.$t
-
-//       const mainPet = petList[0];
-//       const mainName = mainPet.name.$t;
-//       const mainDescription = mainPet.description.$t;
-//       const mainEmail = mainPet.contact.email.$t;
-//       const mainTempMedia = mainPet.media.photos.photo[0].$t;
-//       const mainMedia = mainTempMedia.split('&');
-//       const mainImage = mainMedia[0] + '.jpg'
-//
-//       $('#firstPetContainer').append(`<div class="col s12 m12">
-//       <h5 class="header"></h5>
-//       <div class="card horizontal z-depth-5 hoverable">
-//         <div class="card-image">
-//           <img src ="${mainImage}">
-//             <span class="card-title">${mainName}</span>
-//       </div>
-//       <div class="card-stacked">
-//       <div class="card-content">
-//       <h5>${mainDescription}</h5>
-//       </div>
-//       <div class="card-action">
-//       <a href="mailto: ${mainEmail}">Email The Shelter!</a>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-//       `)
 
       for (let i = 0; i < petList.length; i++) {
         const pet = petList[i]
@@ -79,20 +52,20 @@ $(document).ready(function() {
               </div>
                 <div class="card-action">
   <a class="modal-trigger waves-effect waves-light btn #795548 brown" href="#modal${i}">Learn more!</a>
-
   <div id="modal${i}" class="modal modal-fixed-footer">
     <div class="modal-content">
-    <div class="card">
+    <div class="card small" id="popUp">
     <div class="card-image">
         <img src=${image}>
       </div>
       </div>
-      <h4>${name}: ${age} ${breed} ${sex}</h4>
+      <h4>${name}: ${age} ${breed} (${sex})</h4>
       <p>${description}</p>
-      <h5>Please contact the shelter:  ${email} ${phone}</h5>
+      <h5>Please contact the shelter:  ${email}</h5>
+      <h5>${phone}</h5>
     </div>
     <div class="modal-footer">
-      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat #795548">X</a>
+      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat"><h4>X</h4></a>
     </div>
   </div>
               </div>
@@ -102,7 +75,7 @@ $(document).ready(function() {
       }
       $('.modal-trigger').leanModal();
 
-    console.log('data', data)
+      console.log('data', data)
     })
     .fail(function() {
     })
